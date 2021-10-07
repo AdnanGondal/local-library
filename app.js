041f14bbb1c173ae817541ffa01c83aea9ebab81
Adnan Gondal <adnangondal1@hotmail.com>
2021-10-07 18:06:51 +0100

    mongodb code

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+var mongoose = require("mongoose");
+var mongoDB =
+  "mongodb+srv://addy99:bioeng20@cluster0.a6jio.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
