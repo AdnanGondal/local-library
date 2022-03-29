79f450e873017c187dc1d3b58e8c467d9d04c033
Adnan Gondal <adnangondal1@hotmail.com>
2022-03-29 13:23:22 +0100

    depolyment to heroku setup

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var compression = require('compression');
+var helmet = require('helmet');
 require('dotenv').config();
 
 var indexRouter = require("./routes/index");
@@ -26,6 +28,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, "public")));
 
 // route handling
